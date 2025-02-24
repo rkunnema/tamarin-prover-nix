cda1bc48b38e493942932da4357052a315639759
Robert Künnemann <robert.kuennemann@cispa.saarland>
2025-02-24 10:29:25 +0100

    initial commit.

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "Tamarin";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        hPkgs =
+          pkgs.haskell.packages."ghc966"; # need to match Stackage LTS version
+                                           # from stack.yaml snapshot
+
+        myDevTools = [
+          hPkgs.ghc # GHC compiler in the desired version (will be available on PATH)
+          hPkgs.ghcid # Continuous terminal Haskell compile checker
+          hPkgs.ormolu # Haskell formatter
+          hPkgs.hlint # Haskell codestyle checker
+          hPkgs.hoogle # Lookup Haskell documentation
+          hPkgs.haskell-language-server # LSP server for editor
+          hPkgs.implicit-hie # auto generate LSP hie.yaml file from cabal
+          hPkgs.retrie # Haskell refactoring tool
+          # hPkgs.cabal-install
+          stack-wrapped
+          pkgs.zlib # External C library needed by some Haskell packages
+        ];
+
+        # from https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/science/logic/tamarin-prover/default.nix#L27
+        libraryHaskellDepends = with hPkgs; [
+          # tamarin-prover-utils
+          base64-bytestring
+          blaze-builder
+          list-t
+          dlist
+          exceptions
+          fclabels
+          graphviz
+          safe
+          SHA
+          split
+          syb
+          # tamarin-prover-term
+          attoparsec
+          HUnit
+          # tamarin-prover-theory 
+          aeson
+          aeson-pretty
+          parallel
+          uniplate
+          regex-pcre-builtin
+          regex-posix
+          split
+          # tamarin-prover-sapic
+          raw-strings-qq
+        ];
+
+        # Wrap Stack to work with our Nix integration. We don't want to modify
+        # stack.yaml so non-Nix users don't notice anything.
+        # - no-nix: We don't want Stack's way of integrating Nix.
+        # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
+        # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack"; # will be available as the usual `stack` in terminal
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = myDevTools ++ libraryHaskellDepends;
+
+          # Make external Nix c libraries like zlib known to GHC, like
+          # pkgs.haskell.lib.buildStackProject does
+          # https://github.com/NixOS/nixpkgs/blob/d64780ea0e22b5f61cd6012a456869c702a72f20/pkgs/development/haskell-modules/generic-stack-builder.nix#L38
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
+        };
+      });
+}
